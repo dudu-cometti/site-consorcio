61a17f8c8d80b9aa13318af991c49b8d3917fa8f
dudu-cometti <eduardocometti7@gmail.com>
2025-03-26 08:53:40 -0300

    Correcao com pagina de veiculos funcionando

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Importe suas páginas
 import Home from './pages/Home';
 import Veiculos from './pages/Veiculos';
+import DetalhesVeiculo from './pages/DetalhesVeiculo';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import Perfil from './pages/Perfil';
@@ -15,7 +16,8 @@ function App() {
       <div className="app-container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/veiculos" element={<Veiculos />} />
+          <Route path="/veiculos/:estado/:categoria" element={<Veiculos />} />
+          <Route path="/veiculo/:id" element={<DetalhesVeiculo />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/perfil" element={
